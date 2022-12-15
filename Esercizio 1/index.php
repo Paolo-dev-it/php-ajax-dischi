62356208030737a590445d4d5cd166d625f19e0a
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-12-15 14:14:35 +0100

    Created folders and files. Working on first exercise

---
+++ Esercizio 1/index.php
@@ -0,0 +1,85 @@
+ <?php 
+ 
+  $database = [
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+            'genre' => 'Rock'
+        ],
+        [
+            'title' => 'Live at Wembley 86',
+            'author' => 'Queen',
+            'year' => 1992,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'Brave new World',
+            'author' => 'Iron Maiden',
+            'year' => 2000,
+            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+            'genre' => 'Metal'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+            'genre' => 'Rock'
+        ]
+    ];
+ 
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style.css">
+ </head>
+ <body>
+
+    <div class="container">
+        <div class="row">
+            <?php foreach ($database as $data) { ?>
+                <div class="card" style="width: 18rem;">
+                    <img src= "<?php echo $data['poster'] ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title"><?php echo $data['title'] ?></h3> 
+                        <h5 class="card-title">
+                        <?php echo $data['author'] ?>
+                        </h5>
+                        <p class="card-text">
+                        <?php echo $data['year'] ?>
+                        </p>
+                    </div>
+                </div>
+            
+        <?php } ?>
+        </div>
+    </div>
+    
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+ </body>
+ </html>
+ 
